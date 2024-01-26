979e7e6d8e7e2bf4f46ac32feef35abb2153c058
Zakkon <zakkonzoom@gmail.com>
2024-01-26 23:38:03 +0100

    think i managed to get loading for featureOptSel to work better

--- web/mancer-inspired/js/main.js
+++ web/mancer-inspired/js/main.js
@@ -358,6 +358,7 @@ class CharacterBuilder {
     }
     renderComponents(){
         this.compClass.render();
+        //this.compClass._test_loadFeatureOptSelectsFromSaveFile(this.actor.classes);
         this.compRace.render();
         this.compAbility.render();
         this.compBackground.render();
@@ -369,9 +370,9 @@ class CharacterBuilder {
     }
     async loadFromSave(){
       const delay = ms => new Promise(res => setTimeout(res, ms));
-      await delay(1000);
+      //await delay(1000);
       console.log("waited!");
-      this.compClass._test_loadFeatureOptSelectsFromSaveFile(this.actor.classes);
+      //this.compClass._test_loadFeatureOptSelectsFromSaveFile(this.actor.classes);
       //await this.compClass.pLoadLate();
     }
 
